b649f5786c22ae60d019c717141ff5df31c060dc
Emil Anton <emilbstn@gmail.com>
2022-12-11 08:15:49 -0800

    remove unnecessary whitespace and old console.log calls used in testing

--- js/scripts.js
+++ js/scripts.js
@@ -5,24 +5,19 @@ function Pizza (toppingsList) {
 Pizza.prototype.price = function() {
   const size = document.getElementById('size');
   let pizzaSize = size.options[size.selectedIndex].text;
-
   numToppings = 0;
   for (var k in this.toppingsList) {
     if (this.toppingsList[k] === true) {
       numToppings ++;
     }
   }
-  let toppingsPrice = numToppings * 0.25;
-  let pizzaPrice = ((3/4) * (parseInt(size.value)));
-  console.log(numToppings);
-  console.log(toppingsPrice);
+  let pizzaPrice = ((3/4) * (parseInt(size.value)) + (numToppings * 0.25));
   console.log(pizzaSize);
   console.log(pizzaPrice);
 }
 const myPizza = new Pizza({})
 
 window.addEventListener('load', (event) =>{
-
   const toppingsForm = document.getElementById('toppings-form');
   let topInputs = toppingsForm.getElementsByTagName('input');
   for (let i = 0; i < topInputs.length; i++) { 
@@ -31,11 +26,9 @@ window.addEventListener('load', (event) =>{
         const cBox = this;
         if (myPizza.toppingsList[cBox.id] === false){
           myPizza.toppingsList[cBox.id] = true;
-          console.log(myPizza.toppingsList)
         }
         else {
           myPizza.toppingsList[cBox.id] = false;
-          console.log(myPizza.toppingsList)
         }
       });
       myPizza.toppingsList[topInputs[i].id] = false;
